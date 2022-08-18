12c00cd130bb6075d1cf3749e8f7d7518f4aec9a
이수빈 <980429sb@naver.com>
2022-08-18 16:06:31 +0900

    220818 ArrayQueue All Element Print *focus:index*

--- src/queue/ArrayQueue.java
+++ src/queue/ArrayQueue.java
@@ -1,10 +1,13 @@
 package queue;
 
+import java.util.Arrays;
+
 /*
  * 선형큐의 문제점 <잘못된 포화상태 인식>
  * 1. 원소의 위치가 점진적으로 왼쪽에서 오른쪽으로 이동한다.
  * 2. 즉, enQueue 와 deQueue를 거치며 배열의 앞공간은 비어있음에도 불구하고,
  * 	  rear = n-1인 상태(포화상태)로 인식하여 더 이상의 삽입이 불가능하다.
+ * 3. 따라서, 선형큐에 존재하는 원소만을 출력하려고 한다면 frontIdx+1부터 rearIdx까지로 범위를 지정해주어야 한다 
  *  
  */
 public class ArrayQueue<E> implements Queue<E> {
@@ -12,8 +15,8 @@ public class ArrayQueue<E> implements Queue<E> {
 	private static final int DEAFAULT_CAPACITY = 50;
 
 	public int capacity; // 큐에 담을 수 있는 최대 용량 즉, 현재 큐의 크기
-	private int frontIdx; // 마지막으로 삭제된 원소의 인덱스
-	private int rearIdx; // 마지막으로 저장된 원소의 인덱스
+	public int frontIdx; // 마지막으로 삭제된 원소의 인덱스
+	public int rearIdx; // 마지막으로 저장된 원소의 인덱스
 	private E[] elements; // 스택을 구현하는 배열
 
 	@SuppressWarnings("unchecked")
@@ -71,8 +74,8 @@ public class ArrayQueue<E> implements Queue<E> {
 		if (isFull()) {
 			return false;
 		} else {
-//			this.rearIdx++;
-			this.elements[++rearIdx] = anElement;
+			this.rearIdx++;
+			this.elements[rearIdx] = anElement;
 			return true;
 		}
 
@@ -82,14 +85,22 @@ public class ArrayQueue<E> implements Queue<E> {
 	public E deQueue() {
 		// 스택이 비워져있는지 확인한다
 		// 스택이 isEmpty하면 삭제할 원소가 없다
-		E frontElement = null;
+		/*
+		 * E frontElement = null;
+		 * 
+		 * if (!isEmpty()) { frontElement = this.elements[++frontIdx];
+		 * this.elements[this.frontIdx] = null; // 증가된 frontIdx에 있는 원소를 삭제 } return
+		 * frontElement;
+		 */
 
-		if (!isEmpty()) {
+		if (isEmpty()) {
+			return null;
+		} else {
 			this.frontIdx++;
-			frontElement = this.elements[this.frontIdx];
-			this.elements[this.frontIdx] = null; // 증가된 frontIdx에 있는 원소를 삭제
+			E frontElement = this.elements[frontIdx];
+			return frontElement;
 		}
-		return frontElement;
+
 	}
 
 	@Override
@@ -105,12 +116,27 @@ public class ArrayQueue<E> implements Queue<E> {
 	// 수정필요
 	@Override
 	public E elementAt(int order) {
-		for (int i = 0; i < this.size(); i++) {
+		for (int i = this.frontIdx; i < this.rearIdx + 1; i++) {
 			if (i == order) {
 				return this.elements[i];
 			}
 		}
 		return null;
+	}
+
+	// 출력해야할 인덱스는 front+1 부터 rear까지이다
+	// Arrays.copyOf(원본배열, 복사길이)
+	// -> 0번째부터 두번째 파라미터인 복사길이만큼 복사한다. 즉, 시작인덱스가 0으로 고정되어있다.
+	// -> 선형큐의 경우 0부터 복사한다면 deQueue 했을 때 null값이 출력될 수 있다.
+	// 이를 방지 하기 위하여 시작인덱스를 지정해줄 수 있는 메소드를 사용한다.
+
+	// Arrays.copyOfRange(원본 배열, 시작인덱스, 마지막인덱스+1)
+
+	public void printList() {
+		System.out.print("<Front> ");
+//		System.out.println(Arrays.toString(Arrays.copyOf(elements, size())));
+		System.out.print(Arrays.toString(Arrays.copyOfRange(elements, frontIdx + 1, rearIdx + 1)));
+		System.out.print(" <Rear>\n");
 	}
 
 }
--- src/queue/ArrayQueueTest.java
+++ src/queue/ArrayQueueTest.java
@@ -8,25 +8,30 @@ public class ArrayQueueTest {
 		queue.enQueue("A");
 		queue.enQueue("B");
 		queue.enQueue("C");
+		queue.printList(); // A B C
 
 //		System.out.println(queue.size()); // 3, 현재 들어간 원소 개수 
 //		System.out.println(queue.capacity); // 50, 사용자가 지정하지 않아 자동 초기화된 최대 용량  
 
-		showAllElements(); // A B C
-		System.out.println(queue.front()); // A
-		System.out.println(queue.rear()); // C
-//		System.out.println(queue.deQueue()); // A 삭제 
-		showAllElements(); // B C 출력 되어야 함 ....
+//		System.out.println(queue.front()); // A
+//		System.out.println(queue.rear()); // C
+
+		System.out.println(queue.deQueue()); // A 삭제
+		queue.printList(); // B C
 
 		queue.clear();
-//		showAllElements();
+		queue.printList(); // empty
+
 	}
 
+	// 출력해야할 인덱스는 front+1 부터 rear까지이다.
+	// printList() 와 동일한 결과를 출력한다.
 	public static void showAllElements() {
 		System.out.print("<Front> ");
-		for (int i = 0; i < queue.size(); i++) {
+		for (int i = queue.frontIdx + 1; i < queue.rearIdx + 1; i++) {
 			System.out.print(queue.elementAt(i) + " ");
 		}
 		System.out.print("<Rear>\n");
 	}
+
 }
